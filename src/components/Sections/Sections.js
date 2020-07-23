7342f9ccb61cdaf5c3087a31a63e313b0f57b936
Ryan How <ryanhow1999@yahoo.ca>
2020-07-23 16:16:41 -0400

    responsive design for homepage sections

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -4,7 +4,7 @@ import './Sections.css';
 class Sections extends Component {
     render() {
         return (
-            <div className="sections">
+            <div className="flex justify-around">
                 <a href="/about">
                     <div className="section">
                         <img src={require(`./../../assets/images/about.svg`)} className="ma0" height="400px"></img>
